f5a8db89176df2ab31a01ea18fe007db716506b0
albertalef <albertalef@protonmail.com>
2025-10-01 21:54:42 -0300

    chore: add more one example

--- examples/example-files.rb
+++ examples/example-files.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 
-require 'rubyshell'
+require "rubyshell"
 require "securerandom"
 
 sh do
   mkdir "files"
 
   cd "files" do
-    5.times do |i|
-      echo "#{SecureRandom.alphanumeric(5)} >> #{i}.txt"
-    end
+    5.times { |i| echo "#{SecureRandom.alphanumeric(5)}>>#{i}.txt" }
 
     number_of_files = chain { ls | wc("-l") }.strip
 
@@ -22,3 +20,14 @@ sh do
 
   rm "-rf files"
 end
+
+# Running:
+#
+# ❯ ./examples/example-files.rb
+#
+# Number of files: 5
+# YzN5S
+# igEiz
+# AU5fy
+# rRlDW
+# O6NCB
