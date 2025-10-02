c2b56df9388293773fb1de9f2a0c29d6304cb71f
albertalef <albertalef@protonmail.com>
2025-10-02 00:31:45 -0300

    chore: improve first example file

---
+++ examples/example1.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require "rubyshell"
+require "securerandom"
+
+sh do
+  mkdir "files"
+
+  cd "files" do
+    5.times do |i|
+      chain do
+        echo(SecureRandom.alphanumeric(16)) >> "#{i}.txt"
+      end
+    end
+
+    puts "Number of Files: #{ls.lines.count}"
+
+    ls.each_line do |filename|
+      puts cat(filename)
+    end
+  end
+ensure
+  rm "-rf files"
+end
+
+# Running:
+#
+# ❯ ./examples/example1.rb
+#
+# Number of Files: 5
+# o6Kw8KHvWJnLGSeQ
+# qkRKcZHqu2Moq1se
+# nUPluln9GM1ydtoz
+# rkdYsc1RBhkeN1dq
+# ZPXZMqzYfyFfjPHF
