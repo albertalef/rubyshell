3ff5f098eaeb28670228397ae93d30e75dbb0d6a
Albert Álef <85764913+albertalef@users.noreply.github.com>
2026-01-18 16:22:53 -0300

    18 commands to override (#22)
    * feat: add overwrited commands
    
    * chore: remove irbrc exclude from rubycop
    
    * chore: change rubocop
    
    * chore: remove irbrc from rubocop
    
    * chore: add again the removed files

---
+++ lib/rubyshell/overwrited_commands.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module RubyShell
+  module OverwritedCommands
+    def self.included(mod)
+      mod.extend self
+    end
+
+    def cd(path, &block)
+      Dir.chdir(path, &block)
+    end
+
+    def ls(*args)
+      method_missing(:ls, *args)
+    end
+  end
+end
