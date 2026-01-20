d1eab4319325723737a7a343a6f89d01491241b2
Albert Alef <albertalef@protonmail.com>
2026-01-20 12:14:22 -0300

    fix: create a file to load default irb executions

---
+++ lib/rubyshell/tty/irb.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module RubyShell
+  module Tty
+    module IrbWorkspace
+      def initialize(*args, **kwargs)
+        super
+        @main.extend(RubyShell::Executor)
+      end
+    end
+  end
+end
+
+IRB::WorkSpace.prepend(RubyShell::Tty::IrbWorkspace)
+
+IRB.conf[:IRB_RC] = proc do |ctx|
+  if defined?(IRB::Command) && IRB::Command.respond_to?(:commands)
+    IRB::Command.commands.delete(:cd)
+  elsif defined?(IRB::ExtendCommand) && IRB::ExtendCommand.respond_to?(:commands)
+    IRB::ExtendCommand.commands.delete(:cd)
+  end
+
+  ctx.command_aliases.delete(:cd) if ctx.respond_to?(:command_aliases) && ctx.command_aliases
+end
