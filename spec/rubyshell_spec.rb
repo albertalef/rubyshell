1ddbc7830393d73492c1191823b5b1b2f9d9038a
Albert Alef <albertalef@protonmail.com>
2026-01-22 22:56:36 -0300

    fix: add missing test

--- spec/rubyshell_spec.rb
+++ spec/rubyshell_spec.rb
@@ -290,7 +290,39 @@ RSpec.describe RubyShell do # rubocop:disable Metrics/BlockLength
     end
   end
 
-  it "has a version number" do
-    expect(RubyShell::VERSION).not_to be nil
+  describe "Validating Executable" do
+    context 'when using "new" command' do
+      def subject_method
+        sh do
+          rubyshell "new", "testfile".quoted
+        end
+      end
+
+      it "creates the correct file" do
+        subject_method
+
+        expect(sh.ls.chomp).to eq("testfile.rb")
+      end
+
+      it "creates a file with correct permissions" do
+        subject_method
+
+        expect(sh.ls("-l", "testfile.rb").split.first).to eq("-rwxr-xr-x")
+      end
+    end
+
+    context 'when using "new" command without a filename' do
+      def subject_method
+        sh do
+          rubyshell "new"
+        end
+      end
+
+      it "creates the correct file" do
+        subject_method
+
+        expect(sh.ls.chomp).to eq("new_script.rb")
+      end
+    end
   end
 end
