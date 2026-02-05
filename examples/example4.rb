316cba95023c11c0a79aad5cc356089881b01bf6
Albert Alef <albertalef@protonmail.com>
2026-02-05 19:53:12 -0300

    chore: improve example

--- examples/example4.rb
+++ examples/example4.rb
@@ -5,22 +5,31 @@ require "rubyshell"
 
 # Inspired on: https://www.reddit.com/r/hyprland/comments/1qfrpt2/my_very_lightweight_ocr_setup_with_tesseract/
 sh do
+  # Use Slurp to select on screen, and use grim to print the screen in selected area
   image = grim("-g", slurp.quoted, "-")
 
+  # OCR the image
   content = tesseract(
     "- stdout",
+    c: [
+      "load_system_dawg=1",
+      "load_freq_dawg=1",
+      'tessedit_char_blacklist="¦"'
+    ],
     oem: 1,
     psm: 6,
-    c: ["load_system_dawg=1", "load_freq_dawg=1", 'tessedit_char_blacklist="¦"'],
     _stdin: image
   )
 
+  # Put text on clipboard
   sh("wl-copy", _stdin: content)
 
+  # Send notification
   sh(
     "notify-send",
-    { h: ["string:x-canonical-private-synchronous:ocr", "string:markup-body:1"] },
+    "-h string:x-canonical-private-synchronous:ocr",
+    "-h string:markup-body:1",
     '"OCR copiado"',
-    "'<tt>#{content}</tt>'"
+    content.quoted
   )
 end
